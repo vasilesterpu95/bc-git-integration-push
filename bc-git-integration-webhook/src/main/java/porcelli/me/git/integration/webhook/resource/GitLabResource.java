c17043c35cd44a0ba5fcb49566507eab27a51a2e
vasile.sterpu <vasile.sterpu@datumsolutions.net>
2022-06-17 15:00:44 +0300

    GitlabResource logging

--- bc-git-integration-webhook/src/main/java/porcelli/me/git/integration/webhook/resource/GitLabResource.java
+++ bc-git-integration-webhook/src/main/java/porcelli/me/git/integration/webhook/resource/GitLabResource.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import porcelli.me.git.integration.common.model.Payload;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,15 +20,15 @@ import java.io.InputStream;
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class GitLabResource extends HookResourceBase {
+    private static final Logger logger = LoggerFactory.getLogger(GitLabResource.class);
 
     @POST
     @Path("/")
     public Response post(@HeaderParam("X-Gitlab-Event") String eventType,
                          @Context HttpServletRequest request) {
+        logger.info("GITLAB event {}", eventType);
         try (InputStream in = request.getInputStream()) {
             Payload.EventType type = Payload.EventType.valueOf(eventType.replace(" ", "_").toUpperCase());
-
-//            MergeRequestEvent event = objectMapper.readValue(in, MergeRequestEvent.class);
             JSONObject event = new JSONObject(new JSONTokener(in));
 
             switch (type) {
@@ -42,13 +44,15 @@ public class GitLabResource extends HookResourceBase {
 
             return Response.ok().build();
         } catch (JsonParseException | JsonMappingException e) {
-            e.printStackTrace();
+           logger.error("JSON exception", e);
             throw new InternalServerErrorException(e.getMessage(), Response
                     .status(Response.Status.INTERNAL_SERVER_ERROR)
                     .entity(e.getMessage() + "\n").build(), e);
         } catch (IllegalArgumentException e) {
+            logger.error("Illegal argument exception", e);
             throw new BadRequestException(e);
         } catch (Exception e) {
+            logger.error("Error occurred ", e);
             throw new InternalServerErrorException(Response.status(Response.Status.INTERNAL_SERVER_ERROR)
                                                            .entity(e.getMessage()).build(), e);
         }
