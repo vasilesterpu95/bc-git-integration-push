730b768cc1de411d676d92c6906115861aaa2d87
vasile.sterpu <vasile.sterpu@datumsolutions.net>
2022-07-12 09:36:16 +0300

    business-central user will be created by default

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 /usr/bin/envsubst </opt/jboss/gitremote >/opt/jboss/.gitremote
 /usr/bin/envsubst </opt/jboss/gitremote >/root/.gitremote
+/opt/jboss/wildfly/bin/add-user.sh -a -u user -p password -g user,admin
 
 supervisord -c /opt/jboss/wildfly/bin/supervisor.conf
